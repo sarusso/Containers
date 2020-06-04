5a6f343b6f2a187dde7b89562acf9fc67bb0c757
Stefano Alberto Russo <stefano.russo@gmail.com>
2020-06-04 19:31:01 +0200

    Added CASA Dekstop v5.6.1-8.

---
+++ CasaDesktop/build.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+
+if [ ! -f "Dockerfile" ]; then
+    # TODO: This check is weak: improve me!
+    echo "Please run this script from the AstrocookDesktop folder"
+    exit 1
+fi
+
+# Use --no-cache in case of build problems (i.e. 404 not found)
+docker build . -t casadesktop
+

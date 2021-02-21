e740eec4841c8cc1efb2d3f9dfa701547b88862c
Stefano Alberto Russo <stefano.russo@gmail.com>
2021-02-21 23:21:00 +0100

    Removed Python 2 support, upadated pip and Python dependencies.

--- DevMetaDesktop/files/system_deps.sh
+++ DevMetaDesktop/files/system_deps.sh
@@ -11,17 +11,9 @@ apt-get install curl -y
 # Install get-pip script
 curl -O https://bootstrap.pypa.io/get-pip.py
 
-# Install Python and pip in this order (first Python 3 and then Python 2), or 
-# you will end ap with python defaulting to python2 and pip defaulting to pip3
-# Otherwise, do somethign like "ln -s /usr/local/bin/pip3 /usr/local/bin/pip"
-
 # Install Python3 and Pip3 (python3-distutils required for pip3)
 apt-get install python3 python3-distutils -y 
-python3 get-pip.py 'pip==10.0.1'
-
-# Install Python2 and Pip2
-apt-get install python -y
-python get-pip.py 'pip==10.0.1'
+python3 get-pip.py 'pip==21.0.1'
 
 # Python-tk required by matplotlib/six
 apt-get install python-tk python3-tk -y
